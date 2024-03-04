a0d3a0c7f4c2378d4806f5790f59000e127a0b48
kumaran16 <kumaran16@users.noreply.github.com>
2024-03-04 10:16:34 -0600

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "rg_id_out" {
+  description = "The id of the resource group"
+  value       = azurerm_resource_group.rg.id
+}
+
+output "rg_name_out" {
+  description = "The id of the resource group"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "rg_location_out" {
+  description = "The id of the resource group"
+  value       = azurerm_resource_group.rg.location
+}
