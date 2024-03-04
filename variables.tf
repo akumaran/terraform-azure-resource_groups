62baba14f8905f498647d392e03ff8dfb952e2e2
kumaran16 <kumaran16@users.noreply.github.com>
2024-03-04 10:38:47 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
 variable "name" {
-  description = "A string for the name of the resource group."
+type = string  
+description = "A string for the name of the resource group."
 }
 
 variable "location" {
+  type = string
   description = "(Optional) A string for the location of the resource group."
   default     = "eastus"
 }
 
 variable "tags" {
+  type = map(any)
   description = "(Optional) A key-value map of tags for the resource group."
   default     = {}
 }
