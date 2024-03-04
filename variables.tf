25f1b1b96d8c8a7aa171fed3eee724dd49706040
kumaran16 <kumaran16@users.noreply.github.com>
2024-03-04 10:16:15 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "name" {
+  description = "A string for the name of the resource group."
+}
+
+variable "location" {
+  description = "(Optional) A string for the location of the resource group."
+  default     = "eastus"
+}
+
+variable "tags" {
+  description = "(Optional) A key-value map of tags for the resource group."
+  default     = {}
+}
